f43d95c509ad2060f272a40c92a92cd323e936b4
bipro-b <biprocuet@gmail.com>
2025-11-29 12:44:24 +0600

    try

--- infra/modules/compute/main.tf
+++ infra/modules/compute/main.tf
@@ -8,12 +8,43 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+# IAM role for EC2 so user-data can call AWS (ECR login, sts, etc.)
+resource "aws_iam_role" "ec2_role" {
+  name = "${var.project_name}-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach ECR read-only policy so EC2 can pull images
+resource "aws_iam_role_policy_attachment" "ecr_readonly" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# Instance profile that will be attached to the EC2 instance
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "${var.project_name}-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
 resource "aws_instance" "app" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_group_ids
   key_name               = var.key_name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
   user_data = <<-EOF
     #!/bin/bash
@@ -25,7 +56,6 @@ resource "aws_instance" "app" {
     REGION="${var.region}"
     IMAGE="${var.ecr_repo_url}:${var.image_tag}"
 
-    # login to ecr
     aws ecr get-login-password --region $REGION | \
       docker login --username AWS --password-stdin ${var.ecr_repo_url}
 
